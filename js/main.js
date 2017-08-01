4b4683aa1a737892bb31ea3692411053454446f3
rockindolphin <rockindolphing@gmail.com>
2017-08-01 11:28:50 +0300

    jquery mobile

--- js/main.js
+++ js/main.js
@@ -1,45 +1,43 @@
-$(document).ready(function () {
+function setCssScrollbarWidth(){
+	// Create the measurement node
+	var scrollDiv = document.createElement("div");
+	scrollDiv.className = "scroll__measure";
+	document.body.appendChild(scrollDiv);
 
-	//замена картинок
-	$('img[data-fit="true"]').each(function(){
-		var src = $(this).attr('src');
-		var replacer = document.createElement('div');
-		$(replacer).css('background-image', 'url('+src+')').addClass('image_fit_replacer');
-		$(this).after(replacer);
-		$(this).css('opacity', 0);
+	// Get the scrollbar width
+	var scrollbarWidth = scrollDiv.offsetWidth - scrollDiv.clientWidth;
+
+	// Delete the DIV 
+	document.body.removeChild(scrollDiv);
+
+	//Set css custom property value
+	var body = document.querySelector('body');
+	body.style.setProperty('--scrollbar-width', scrollbarWidth+'px');
+	if( scrollbarWidth > 0 ){
+		$('.scroll--hidden').each(function(){
+			$(this).css({
+				marginRight: -scrollbarWidth
+			});
+		});			
+	}
+	return scrollbarWidth;
+}
+
+$(document).bind('mobileinit', function(){
+	$.extend(  $.mobile , {
+		defaultPageTransition: 'flip'
 	});
+});
+
+$(document).on( 'pageshow', function(e) {
+	//pagecreate->pageinit->pageshow
+	setCssScrollbarWidth();	
 	
 	$('img[data-bg="true"]').each(function(){
 		var src = $(this).attr('src');
 		$(this).parent().css('background-image', 'url('+src+')');
 		$(this).css('opacity', 0);
 	}); 	
-
-
-	function setCssScrollbarWidth(){
-		// Create the measurement node
-		var scrollDiv = document.createElement("div");
-		scrollDiv.className = "scroll__measure";
-		document.body.appendChild(scrollDiv);
-
-		// Get the scrollbar width
-		var scrollbarWidth = scrollDiv.offsetWidth - scrollDiv.clientWidth;
-
-		// Delete the DIV 
-		document.body.removeChild(scrollDiv);
-
-		//Set css custom property value
-		var body = document.querySelector('body');
-		body.style.setProperty('--scrollbar-width', scrollbarWidth+'px');
-		if( scrollbarWidth > 0 ){
-			$('.scroll--hidden').each(function(){
-				$(this).css({
-					marginRight: -scrollbarWidth
-				});
-			});			
-		}
-	}
-
-	setCssScrollbarWidth();	
-
+		
 });
+
